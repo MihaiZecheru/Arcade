8df25076a0fab1650a23c3c721cd616aaaa30e76
Mihai Zecheru <zecheruchris@gmail.com>
2023-08-13 18:01:20 -0700

    Add Player class - ws prop for communication

---
+++ server/src/models/player.ts
@@ -0,0 +1,18 @@
+import { UserID } from "./user";
+
+export default class Player {
+  /**
+   * The ID of the user in the database
+   */
+  user_id: UserID;
+
+  /**
+   * The number of the player, ie. player 1, player 2, etc.
+   */
+  player_number: number;
+
+  /**
+   * Websocket connection the player has made
+   */
+  ws: any;
+}
