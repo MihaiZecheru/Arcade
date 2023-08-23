61ec957f959eb3c8ec12bc213c2c41a9b52edcc3
Mihai Zecheru <zecheruchris@gmail.com>
2023-08-22 23:37:12 -0700

    Use GetArcadeUserSync

--- games/Rock Paper Scissors (CLI)/Main.cs
+++ games/Rock Paper Scissors (CLI)/Main.cs
@@ -1,13 +1,16 @@
-﻿namespace RockPaperScissorsCLI;
+﻿using ArcadeLib;
+
+namespace RockPaperScissorsCLI;
 
 public class RockPaperScissors
 {
     public static void Main(string[] args)
     {
         ArcadeLib.UserID UserID = ArcadeLib.Auth.LoginPrompt();
-        
+        ArcadeUser user = DatabaseAPI.GetArcadeUserSync(UserID);
+
         Console.Clear();
-        Console.WriteLine(UserID);
+        Console.WriteLine(user);
         Console.ReadKey();
     }
 }
