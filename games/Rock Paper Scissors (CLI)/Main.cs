a38ccc1522cfe1997f159aaaab585772732ec475
Mihai Zecheru <zecheruchris@gmail.com>
2023-08-22 20:47:27 -0700

    Start work on Rock Paper Scissors

---
+++ games/Rock Paper Scissors (CLI)/Main.cs
@@ -0,0 +1,14 @@
+﻿using Arcade;
+
+namespace RockPaperScissorsCLI;
+
+public class RockPaperScissors
+{
+    public static void Main(string[] args)
+    {
+        string UserID = Arcade.Auth.Login();
+        Console.Clear();
+        Console.WriteLine(UserID);
+        Console.ReadKey();
+    }
+}
