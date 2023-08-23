186e9cbd75aa56dc55acdc2c766b5446f2cc77a4
Mihai Zecheru <zecheruchris@gmail.com>
2023-08-22 23:05:25 -0700

    Add methods: increase / decrease token balance

--- games/ArcadeLib/User.cs
+++ games/ArcadeLib/User.cs
@@ -20,7 +20,7 @@ public class User
     /// <summary>
     /// The amount of tokens the user has
     /// </summary>
-    public int Balance { get; set; }
+    private int _balance;
 
     /// <summary>
     /// The user's email address
@@ -37,4 +37,57 @@ public class User
     /// </summary>
     public string Joined { get; }
 
+    /// <summary>
+    /// Initialize an existing user
+    /// </summary>
+    public User(ArcadeLib.UserID user_id, string username, string password, int balance, string email, string birthday, string joined)
+    {
+        this.ID = user_id;
+        this.Username = username;
+        this.Password = password;
+        this._balance = balance;
+        this.Email = email;
+        this.Birthday = birthday;
+        this.Joined = joined;
+    }
+
+    /// <summary>
+    /// Get the user's token balance
+    /// </summary>
+    public int GetBalance()
+    {
+        return _balance;
+    }
+
+    /// <summary>
+    /// Add tokens to the user's balance
+    /// </summary>
+    /// <param name="amount">The amount of tokens to add</param>
+    /// <returns>The user's new balance</returns>
+    public int IncreaseBalance(int amount)
+    {
+        _balance += amount;
+        return _balance;
+    }
+
+    /// <summary>
+    /// Deduct tokens from the user's balance
+    /// </summary>
+    /// <param name="amount">The amount of tokens to deduct</param>
+    /// <returns>The user's new balance</returns>
+    /// <exception cref="Exception">Insufficient token balance</exception>
+    public int DecreaseBalance(int amount)
+    {
+        if (_balance < amount)
+            throw new Exception("Insufficient token balance");
+
+        _balance -= amount;
+        return _balance;
+    }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        return $"ID: {ID}\nUsername: {Username}\nPassword: {Password}\nBalance: {Balance}\nEmail: {Email}\nBirthday: {Birthday}\nJoined: {Joined}";
+    }
 }
