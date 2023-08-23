4bf2b59d92c34da7ed1b790e8070a66239a13963
Mihai Zecheru <zecheruchris@gmail.com>
2023-08-22 23:57:32 -0700

    Add WaitOnExit() method

--- games/ArcadeLib/Misc.cs
+++ games/ArcadeLib/Misc.cs
@@ -7,5 +7,18 @@ namespace ArcadeLib;
 /// </summary>
 public static class Misc
 {
+    /// <summary>
+    /// Launch the exit sequence for an Arcade game -- clear the console and wait for the enter key to be pressed
+    /// </summary>
+    public static void WaitOnExit()
+    {
+        Console.Clear();
+        AnsiConsole.Write(TextColor.Gold("* Press ENTER to exit").Centered());
 
+        while (true)
+        {
+            ConsoleKeyInfo keyinfo = Console.ReadKey(true);
+            if (keyinfo.Key == ConsoleKey.Enter) break;
+        }
+    }
 }
