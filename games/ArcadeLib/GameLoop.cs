d8f479eed602088f7c6b2e550eec19885de71a98
Mihai Zecheru <zecheruchris@gmail.com>
2023-08-23 21:05:58 -0700

    Setup gameloop methods

--- games/ArcadeLib/GameLoop.cs
+++ games/ArcadeLib/GameLoop.cs
@@ -2,5 +2,28 @@
 
 public static class GameLoop
 {
+    /// <summary>
+    /// Present the user with the Main Menu and return his choice. Uses the <paramref name="gamename"/> as a header
+    /// </summary>
+    /// <param name="gamename">The name of the game the menu is being used for</param>
+    /// <returns>The user's choice from the menu</returns>
+    public static ArcadeLib.MenuOption MainMenu(string gamename)
+    {
+        return ArcadeLib.MenuOption.PLAY_SOLO; //TODO: Implement
+        return ArcadeLib.MenuOption.PLAY_MULTIPLAYER;
+    }
 
+    public static ArcadeLib.Rooms.RockPaperScissorsRoom RoomMenuGetRoom()
+    {
+        return null; //TODO: Implement
+    }
+
+    /// <summary>
+    /// Prompts the user for a wager >= 1
+    /// </summary>
+    /// <returns>The amount of tokens the user would like to bet, must be at least 1</returns>
+    public static int Bet()
+    {
+        return 0; //TODO: Implement
+    }
 }
--- games/Rock Paper Scissors (CLI)/Main.cs
+++ games/Rock Paper Scissors (CLI)/Main.cs
@@ -1,4 +1,5 @@
 ﻿using ArcadeLib;
+using ArcadeLib.Rooms;
 
 namespace RockPaperScissorsCLI;
 
@@ -17,6 +18,34 @@ public static class RockPaperScissors
     public static void Main(string[] args)
     {
         ArcadeLib.Auth.Login(out User);
+        GameLoop();
         ArcadeLib.Misc.WaitOnExit();
+    }
+
+    public static void GameLoop()
+    {
+        ArcadeLib.MenuOption choice = ArcadeLib.GameLoop.MainMenu("Rock Paper Scissors");
+        
+        if (choice == ArcadeLib.MenuOption.PLAY_SOLO)
+        {
+            int bet = ArcadeLib.GameLoop.Bet();
+            PlayCPU(bet);
+        }
+        else if (choice == ArcadeLib.MenuOption.PLAY_MULTIPLAYER)
+        {
+            // No need to ask for bet because the room comes with a bet already set
+            ArcadeLib.Rooms.RockPaperScissorsRoom room = ArcadeLib.GameLoop.RoomMenuGetRoom();
+            PlayMultiplayer(room);
+        }
+    }
+
+    public static void PlayCPU(int bet)
+    {
+
+    }
+
+    public static void PlayMultiplayer(ArcadeLib.Rooms.RockPaperScissorsRoom room)
+    {
+
     }
 }
