dc98b992ecfaf94c793ed8d8ad388c18562bfadb
Mihai Zecheru <zecheruchris@gmail.com>
2023-08-27 13:36:13 -0700

    Remove debug statement

--- server/src/routes/main/register.ts
+++ server/src/routes/main/register.ts
@@ -4,7 +4,6 @@ import User, { USER_STARTING_BALANCE } from "../../models/user";
 export default async function register(req: any, res: any): Promise<void> {
   try {
     const { username, password, email, birthday } = req.body;
-    console.log(req.body, Object.keys(req.body).length)
     if (Object.keys(req.body).length > 4) throw new Error("Too many fields in data");
     const user_id = await User.generate_id();
     Database.post("Users", { user_id, username, password, balance: USER_STARTING_BALANCE.toString(), email, birthday, joined: new Date().toLocaleDateString() });
