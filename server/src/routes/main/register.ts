50d3747160c676c3d3cf83bb535b9ab84a350fcf
Mihai Zecheru <zecheruchris@gmail.com>
2023-08-27 12:40:48 -0700

    Add error for too many fields

--- server/src/routes/main/register.ts
+++ server/src/routes/main/register.ts
@@ -4,10 +4,12 @@ import User, { USER_STARTING_BALANCE } from "../../models/user";
 export async function register(req: any, res: any) {
   try {
     const { username, password, email, birthday } = req.body;
+    console.log(req.body, Object.keys(req.body).length)
+    if (Object.keys(req.body).length > 4) throw new Error("Too many fields in data");
     const user_id = await User.generate_id();
     Database.post("Users", { user_id, username, password, balance: USER_STARTING_BALANCE.toString(), email, birthday, joined: new Date().toLocaleDateString() });
     return res.status(200).send(user_id);
   } catch (err) {
-    return res.status(400).send(err);
+    return res.status(400).send(err.message);
   }
 }
