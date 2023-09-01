ea606f2b17f4e83728cbc1f77677a28dd0e2bfaf
Mihai Zecheru <zecheruchris@gmail.com>
2023-08-31 18:51:01 -0700

    FIx error with res.code() not being a func

--- server/src/routes/rps/create_room.ts
+++ server/src/routes/rps/create_room.ts
@@ -5,10 +5,10 @@ export default async function create_room(req: any, res: any): Promise<void> {
     const wager: number = req.body.wager;
     if (wager <= 0) throw new Error("Wager must be greater than 0");
     if (Math.floor(wager) !== wager) throw new Error("Wager must be a whole number");
-    return res.code(200).send(Server.rps_create_room(wager)); // returns RoomID
+    return res.status(200).send(Server.rps_create_room(wager)); // returns RoomID
   } catch (err) {
     if (err.message === "Wager must be greater than 0" || err.message === "Wager must be a whole number")
-      return res.code(400).send(err.message);
-    return res.code(500).send("Internal server error");
+      return res.status(400).send(err.message);
+    return res.status(500).send("Internal server error");
   }
 }
