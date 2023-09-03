01654d33201981efa6a2f08b768aaec2eb57d55f
Mihai Zecheru <zecheruchris@gmail.com>
2023-09-02 18:57:19 -0700

    Remove accidental async

--- server/__tests__/express.main.login.test.ts
+++ server/__tests__/express.main.login.test.ts
@@ -27,26 +27,26 @@ describe('Test the main.login function -- log the user into the database', () =>
   const res = { text: '', send: (x: any) => { res.text = x }, statusCode: null, status: (x: any) => { res.statusCode = x; return res; } };
   const user_id: UserID = "aa4d7f07-b82a-4b3b-bbf3-382d431b907b";
 
-  test('login successfully with correct credentials', async () => {
-    await router.main.login(req, res);
+  test('login successfully with correct credentials', () => {
+    router.main.login(req, res);
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe(user_id);
   });
 
-  test('login with incorrect password - should throw error', async () => {
-    await router.main.login({ body: { username: req.body.username, password: 'wrong password' } }, res);
+  test('login with incorrect password - should throw error', () => {
+    router.main.login({ body: { username: req.body.username, password: 'wrong password' } }, res);
     expect(res.statusCode).toBe(400);
     expect(res.text).toBe('Incorrect password');
   });
 
-  test('login with non-existing user - should throw error', async () => {
-    await router.main.login({ body: { username: 'wrong username', password: req.body.password } }, res);
+  test('login with non-existing user - should throw error', () => {
+    router.main.login({ body: { username: 'wrong username', password: req.body.password } }, res);
     expect(res.statusCode).toBe(400);
     expect(res.text).toBe('User not found');
   });
 
-  test('login with too many fields - should throw error', async () => {
-    await router.main.login({ body: { username: req.body.username, password: req.body.password, extra: 'field' } }, res);
+  test('login with too many fields - should throw error', () => {
+    router.main.login({ body: { username: req.body.username, password: req.body.password, extra: 'field' } }, res);
     expect(res.statusCode).toBe(400);
     expect(res.text).toBe('Too many fields in data');
   });
