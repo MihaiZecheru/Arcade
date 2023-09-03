9e9c7705fe449fe2889061175664f848ccf78aa4
Mihai Zecheru <zecheruchris@gmail.com>
2023-09-02 20:25:11 -0700

    Test for throwing the correct errors

--- server/__tests__/models.RoomBaseClass.class.test.ts
+++ server/__tests__/models.RoomBaseClass.class.test.ts
@@ -49,8 +49,46 @@ describe('Test the RoomBaseClass', () => {
     expect(player?.user_id).toBe(player2_id);
   });
 
+  test('player_count method', () => {
+    room.add_player(player1_id, {});
+    room.add_player(player2_id, {});
+    expect(room.player_count()).toBe(2);
+  });
+
   test('room properties', () => {
     expect(room.room_id).toBe(room_id);
     expect(room.wager).toBe(100);
+  });
+
+  test('add player already in room', () => {
+    room.add_player(player1_id, {});
+    expect(() => room.add_player(player1_id, {}))
+      .toThrow(`User '${player1_id}' is already in room '${room.room_id}'`);
+  });
+
+  test('add player to full room', () => {
+    room.add_player(player1_id, {});
+    room.add_player(player2_id, {});
+    expect(() => room.add_player("d111b9b8-7a8c-4f54-8239-62515df69caf", {})).toThrow(`Room '${room.room_id}' is full`);
+    expect(room.player_count()).toBe(2);
+  });
+
+  test('remove player from an empty room', () => {
+    const emptyRoom = new RoomBaseClass(uuid(), 2);
+    expect(() => emptyRoom.remove_player('d111b9b8-7a8c-4f54-8239-62515df69caf')).toThrow(`User 'd111b9b8-7a8c-4f54-8239-62515df69caf' is not in room '${room.room_id}'`);
+    expect(emptyRoom.player_count()).toBe(0);
+  });
+
+  test('get_player_by_number method with invalid player number', () => {
+    room.add_player(player1_id, {});
+    expect(() => room.get_player_by_number(0)).toThrow(`There is currently no player number '0' in room '${room.room_id}`);
+    expect(() => room.get_player_by_number(2)).toThrow(`There is currently no player number '2' in room '${room.room_id}`);
+    const player = room.get_player_by_number(1);
+    expect(player.user_id).toBe(player1_id);
+  });
+
+  test('get_player_by_user_id method with non-existing user ID', () => {
+    room.add_player(player1_id, {});
+    expect(() => room.get_player_by_user_id('d111b9b8-7a8c-4f54-8239-62515df69caf')).toThrow(`Player 'd111b9b8-7a8c-4f54-8239-62515df69caf' is not in room '${room.room_id}'`);
   });
 });
