28597b49aac28e94ea090938ef69e163748b8037
Mihai Zecheru <zecheruchris@gmail.com>
2023-09-02 19:41:13 -0700

    Unit test for RoomBaseClass

--- server/__tests__/models.RoomBaseClass.class.test.ts
+++ server/__tests__/models.RoomBaseClass.class.test.ts
@@ -2,9 +2,8 @@ import { RPSRoom } from '../src/models/rooms';
 import { describe, test, expect, beforeEach } from '@jest/globals';
 import uuid from '../src/models/ID';
 import { UserID } from '../src/models/user';
-import Server from '../src/server';
 
-describe('Test the RPSRoom class', () => {
+describe('Test the RoomBaseClass', () => {
   // Create sample players and an RPSRoom instance for testing
   const player1_id: UserID = 'aa4d7f07-b82a-4b3b-bbf3-382d431b907b';
   const player2_id: UserID = 'c1eab2fe-797b-4a9f-ba48-ea155205044b';
@@ -49,17 +48,6 @@ describe('Test the RPSRoom class', () => {
     
     const player = room.get_player_by_user_id(player2_id);
     expect(player?.user_id).toBe(player2_id);
-  });
-
-  test('set_player_choice & get_player_choice methods', () => {
-    room.add_player(player1_id, {});
-    room.add_player(player2_id, {});
-
-    room.set_player_choice(player1_id, 'rock');
-    room.set_player_choice(player2_id, 'scissors');
-
-    expect(room.get_player_choice(player1_id)).toBe('rock');
-    expect(room.get_player_choice(player2_id)).toBe('scissors');
   });
 
   test('room properties', () => {
