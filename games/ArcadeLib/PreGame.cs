0c3080ff116b21ee05178d2ba149c55b020cc512
Mihai Zecheru <zecheruchris@gmail.com>
2023-09-11 00:31:53 -0700

    Work on menus, do room selection and part of main menu

--- games/ArcadeLib/PreGame.cs
+++ games/ArcadeLib/PreGame.cs
@@ -30,22 +30,45 @@ public static class PreGame
     /// <summary>
     /// Prompt the user to choose a room to join, return the selected room
     /// </summary>
-    /// <param name="room_type">The type of room/the name of the game, ex: RPS, HiLo, etc.</param>
+    /// <param name="room_type">The type of room/the name of the game, ex: 'rps', 'hilo', etc.</param>
     /// <returns>The ID of the room the user wants to join</returns>
-    public static async Task<ArcadeLib.UUID> RoomMenu(string room_type)
+    public static async Task<ArcadeLib.UUID> SelectRoom(string room_type)
     {
-        List<ArcadeLib.Rooms.IRoom> rooms;
+        List<ArcadeLib.Rooms.IRoom> rooms = new List<ArcadeLib.Rooms.IRoom>();
         switch (room_type)
         {
-            case "RPS":
-                rooms = await ArcadeLib.Rooms.RockPaperScissorsRoom.GetRooms();
+            case "rps":
+                rooms.AddRange(await ArcadeLib.Rooms.RockPaperScissorsRoom.GetRooms());
                 break;
 
             default:
-                throw new Exception($"No game exists with the name '{room_type}'. What game are you trying to get rooms for?")
+                throw new Exception($"No game exists with the name '{room_type}'. What game are you trying to get rooms for?");
         }
 
-        return null; //TODO: Implement
+        List<string> options = new List<string>() { "Create Room" };
+        rooms.ForEach((ArcadeLib.Rooms.IRoom room) => options.Add(room.id.ToString()));
+        
+        var selected_room_id = AnsiConsole.Prompt(new SelectionPrompt<string>()
+            .Title("Join a room or create your own")
+            .AddChoices(options)
+        );
+
+        if (selected_room_id == "Create Room")
+        {
+            return await CreateRoomPrompt(room_type);
+        }
+
+        return selected_room_id;
+    }
+
+    public static async Task<ArcadeLib.UUID> CreateRoomPrompt(string room_type)
+    {
+        var wager = AnsiConsole.Prompt(
+            new TextPrompt<int>("Set a wager for the room: ")
+                .Validate(bet => bet >= 1 ? ValidationResult.Success() : ValidationResult.Error("Your wager must be at least 1 token")
+        ));
+
+        return await ArcadeLib.ArcadeServerAPI.CreateRoom(room_type, wager);
     }
 
     /// <summary>
