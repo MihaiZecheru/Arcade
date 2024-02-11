8ab84b07aa89ea9f5e27a75d808d1cf1215b4c01
Mihai Zecheru <zecheruchris@gmail.com>
2024-02-11 11:44:08 -0800

    Fix stupid typo causing problems

--- server/src/models/ID.ts
+++ server/src/models/ID.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid_v4 } from 'uuid';
 
-export type ID = "`${string}-${string}-4${string}-${string}-${string}`";
+export type ID = `${string}-${string}-4${string}-${string}-${string}`;
 
 export default function uuid(): ID {
   const _uuid: ID = uuid_v4() as ID;
