c5770f82dc554bd14400a285990fa7b62d03d7d0
Mihai Zecheru <zecheruchris@gmail.com>
2024-02-17 12:46:22 -0800

    Add: Log game in database

--- server/src/routes/rps/close_room.ts
+++ server/src/routes/rps/close_room.ts
@@ -1,3 +1,4 @@
+import Database from "../../../mdb_local";
 import User, { UserID } from "../../models/user";
 import Server, { RoomID } from "../../server";
 
@@ -8,9 +9,16 @@ export default function close_room(req: any, res: any) {
   const loser: UserID = req.body.loser;
 
   if (!room_id) return res.status(400).send('room_id is required');
-  if (Server.room_exists("rps", room_id)) return res.status(400).send(`${game} room with ID ${room_id} not found`);
+  if (Server.room_exists("rps", room_id)) return res.status(400).send(`rps room with ID ${room_id} not found`);
 
-  User.get_user(winner).increase_wallet_balance(wager);
+  // Log game
+  Database.post("GameLog", { game: "rps", room_id, winner, wager: wager.toString(), timestamp: Date.now().toString() });
+  
+  // Update balances
+  User.get_user(winner).increase_wallet_balance(wager * 2); // winner gets their wager back plus the loser's wager
+  // loser balance does not need to be altered as both players pay to join the room
+
+  // Update stats
 
   Server.rps_delete_room(room_id);
   res.status(200).send(`rps room with ID ${room_id} closed`);
