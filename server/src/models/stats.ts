8106beea8434e55e8ada5f4c93c5ba302c3ba91d
Mihai Zecheru <zecheruchris@gmail.com>
2024-02-17 13:49:28 -0800

    Add stats to DB

---
+++ server/src/models/stats.ts
@@ -0,0 +1,18 @@
+import { RoomID } from "../server";
+import { UserID } from "./user";
+
+interface IStats {
+  user_id: UserID;
+  wins: number;
+  losses: number;
+  total_wagered: number;
+  winnings: number;
+  money_lost: number;
+  rooms_played_in: Array<RoomID>;
+}
+
+export interface IStatsRPS extends IStats {
+  rocks: number;
+  scissors: number;
+  papers: number;
+}
