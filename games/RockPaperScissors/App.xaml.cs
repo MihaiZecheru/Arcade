e0238ceac07567adcd2b41b7f12aa0567ed413a6
Mihai Zecheru <zecheruchris@gmail.com>
2024-03-18 22:42:04 -0700

    Create RockPaperScissors wpf app

---
+++ games/RockPaperScissors/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace RockPaperScissors;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        RockPaperScissors.MainWindow mainWindow = new();
+        mainWindow.NavigateToPage(new Pages.CreateOrJoinRoomPage());
+        mainWindow.Show();
+    }
+}
---
+++ games/RockPaperScissors/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RockPaperScissors;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void NavigateToPage(Page page)
+    {
+        // Navigate to page
+        Content = page;
+        // Set window title to page title
+        Title = page.Title;
+    }
+}
