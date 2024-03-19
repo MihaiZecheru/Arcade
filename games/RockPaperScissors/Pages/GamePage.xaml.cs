4228aaeded63849f4da846a8e87b5473079faa17
Mihai Zecheru <zecheruchris@gmail.com>
2024-03-18 22:43:37 -0700

    Add page for the game itself

---
+++ games/RockPaperScissors/Pages/GamePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace RockPaperScissors.Pages;
+
+/// <summary>
+/// Interaction logic for Pages/GamePage.xaml
+/// </summary>
+public partial class GamePage : Page
+{
+    public GamePage()
+    {
+        InitializeComponent();
+    }
+}
